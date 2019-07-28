2a0336a822a5ff7f00be271c9f5da70b491b6688
dom huh <40368238+domhuh@users.noreply.github.com>
2019-07-28 18:46:25 -0400

    init setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="nbml",
+    version="0.0.1",
+    author="Dom Huh",
+    author_email="dhuh137@gmail.com",
+    description="Various tools used for machine learning",
+    url="https://github.com/netbrainml/nbml",
+    packages=setuptools.find_packages(),
+    )

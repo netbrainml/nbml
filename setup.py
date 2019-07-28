fc28aebd4ae6222e946a6f7a3bbbdadefe6c2729
dom huh <40368238+domhuh@users.noreply.github.com>
2019-07-28 18:55:19 -0400

    inits

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ import setuptools
 setuptools.setup(
     name="nbml",
     version="0.0.1",
+    packages=setuptools.find_packages(),
+
     author="Dom Huh",
     author_email="dhuh137@gmail.com",
     description="Various tools used for machine learning",
     url="https://github.com/netbrainml/nbml",
-    packages=setuptools.find_packages(),
-    install_requires=['torch>=1.0', 'fastai', 'tensorflow'],
-
     )

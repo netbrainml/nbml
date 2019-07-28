b39ca2ef999144b149ae563fe4c0fa7b446020c9
dom huh <40368238+domhuh@users.noreply.github.com>
2019-07-28 14:54:52 -0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,2 +1,2 @@
 def shapes(*x):
-    for idx, item in enumerate(x): print(f"arg_{idx}: {item.shape}")
+    for idx, item in enumerate(x): print(f"arg_{idx}: {item.shape}") if hasattr(item, "shape") else print(f"arg_{idx}: {len(item)}")

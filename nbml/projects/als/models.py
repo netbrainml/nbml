44b833ed8543a2f85f50628c41ffda48832d300d
dom huh <40368238+domhuh@users.noreply.github.com>
2019-07-31 11:14:59 -0400

    Update models.py

--- nbml/projects/als/models.py
+++ nbml/projects/als/models.py
@@ -109,3 +109,32 @@ class StackPool(nn.Module):
         self.avg = nn.AdaptiveAvgPool3d(size)
     def __call__(self, X):
         return torch.cat((self.max(X), self.avg(X)), dim=1)
+
+
+def ConvRelu(in_c,out_c,ks,stride,padding=0):
+    conv1 = nn.Conv3d(in_c, out_c, ks, stride, padding)
+    return nn.Sequential(conv1,nn.ReLU())
+
+class C3DBlock(BasicTrainableClassifier):
+    def __init__(self,nc,nf,padding=0):
+        super().__init__()
+        self.block = nn.Sequential(ConvRelu(nc,nf,3,1,padding),
+                                   ConvRelu(nf,nf,3,1,padding),
+                                   nn.MaxPool3d(2,2, padding, ceil_mode=True))
+    def forward(self,x): return self.block(x)
+
+class C3D(BasicTrainableClassifier):
+    def __init__(self, nc, no):
+        super().__init__()
+        self.cp_model = nn.Sequential(ConvRelu(nc, 64, 3, 1, 1),
+                                      nn.MaxPool3d((1,2,2),(1,2,2)),
+                                      ConvRelu(64, 128, 3, 1, 1),
+                                      nn.MaxPool3d((2,2,2),2,(1,0,0)),
+                                      C3DBlock(128,256,padding=(1,0,0)),
+                                      C3DBlock(256,512,padding=1),
+                                      C3DBlock(512,512,padding=(1,0,0)),
+                                      C3DBlock(512,512,padding=1))
+        self.fc = nn.Sequential(nn.Linear(4096,4096),
+                                nn.Linear(4096,no))
+    def forward(self, x):
+        return self.fc(torch.flatten(self.cp_model(x), start_dim=1))

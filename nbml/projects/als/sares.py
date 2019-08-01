22f822d382a1a933840c02a5a338e52b709230f3
dom huh <40368238+domhuh@users.noreply.github.com>
2019-08-01 19:17:32 -0400

    Update sares.py

--- nbml/projects/als/sares.py
+++ nbml/projects/als/sares.py
@@ -8,7 +8,8 @@ def init_cnn(m):
     if isinstance(m, (nn.Conv3d,nn.Conv2d,nn.Linear)): nn.init.kaiming_normal_(m.weight)
     for l in m.children(): init_cnn(l)
 
-
+class noop(Module):
+    def __call__(self, x): return x
 class StackPool(nn.Module):
     def __init__(self, size):
         super().__init__()
@@ -16,9 +17,11 @@ class StackPool(nn.Module):
         self.avg = nn.AdaptiveAvgPool3d(size)
     def __call__(self, X):
         return torch.cat((self.max(X), self.avg(X)), dim=1)
-
-class ResBlock(Module):
+class ResBlockA(Module):
+    def __call__(self, x): return self.act(self.layer(x)+self.idlayer(x))
+class ResBlockC(Module):
     def __call__(self, x): return self.act(self.op((torch.cat([self.layer(x),self.idlayer(x)],dim=2))))
+
 def P3Dconv(ni,nf,stride=1,padding=1, s=True, t=False):
     return nn.Conv3d(ni,nf,kernel_size=(3,1,1) if not s or t else (1,3,3),stride=1,
                      padding=padding,bias=False)
@@ -30,7 +33,8 @@ class SAconv3d(Module):
         self.saconv = saconv2d(nc,nf)
     def __call__(self, x):
         return torch.cat([self.saconv(img.squeeze()).transpose(1,0)[None,:] for img in x])
-class P3Da(ResBlock):
+
+class aP3Da(ResBlockC):
     def __init__(self, ni, nf, nc,**kwargs):
         super().__init__()
         self.layer = nn.Sequential(P3Dconv(ni,nf,padding=0, s=True),
@@ -38,15 +42,23 @@ class P3Da(ResBlock):
         self.idlayer = SAconv3d(nc, nf)
         self.op = nn.Conv3d(nf,nf,3,**kwargs)
         self.act = nn.ReLU(inplace=True)
+class P3Da(ResBlockA):
+    def __init__(self, ni, nf,**kwargs):
+        super().__init__()
+        self.layer = nn.Sequential(P3Dconv(ni,nf,padding=0, s=True),
+                                   P3Dconv(nf,nf,padding=1, t=True))
+        self.idlayer = nn.Conv3d(ni, nf,3, **kwargs)
+        self.act = nn.ReLU(inplace=True)
+
 class FlattenDim(Module):
     def __init__(self, dim): self.dim=dim
     def __call__(self,x): return torch.flatten(x, start_dim=self.dim)
 class P3DaModel(BasicTrainableClassifier):
     def __init__(self, ni, nc, no, **kwargs):
         super().__init__(**kwargs)
-        self.model = nn.Sequential(P3Da(ni, 16, nc, padding=(1,0,0)),
+        self.model = nn.Sequential(aP3Da(ni, 16, nc, padding=(1,0,0)),
                                    nn.MaxPool3d((1,2,2),(1,2,2),(0,1,1)),
-                                   P3Da(16, 32, ni+nc, padding=(0,1,1)),
+                                   P3Da(16, 32, padding=(0,1,1)),
                                    nn.MaxPool3d(2,2),
                                    nn.Conv3d(32,64,3),
                                    nn.MaxPool3d(2,2,padding=(0,1,1)),

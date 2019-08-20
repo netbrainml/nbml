5650ae54cb1a69386dcc465134c71a2efabdae01
domhuh <dhuh137@gmail.com>
2019-08-19 20:13:52 -0400

    Update utils.py

--- nbml/workshops/dog_breed/utils.py
+++ nbml/workshops/dog_breed/utils.py
@@ -4,7 +4,9 @@ import torch
 from tqdm import tqdm
 from torch.utils.data import DataLoader, TensorDataset
 from ...pytorch import *
-
+from collections import Counter
+import operator
+import matplotlib.pyplot as plt
 
 def get_data(path=None, full=False, top=10, **_):
     df = pd.read_csv(path+"labels.csv")
@@ -37,3 +39,23 @@ def nptotorch(x,y, split=0.6, subset=None, verbose=True, nc=120, bs=256, **_):
 
 def load_data(**kwargs):
     return nptotorch(*get_numpy(*get_data(**kwargs), **kwargs), **kwargs)
+
+
+def getHist(train, valid, nc=120):
+    plt.figure(figsize=(8, 4) if nc<21 else (50, 25))
+    train_c = dict(Counter(train)); valid_c = dict(Counter(valid))
+    for cl in range(nc):
+        if cl not in train_c: train_c[cl] = 0
+        if cl not in valid_c: valid_c[cl] = 0
+    tlabels, tvalues = [list(m) for m in zip(*sorted(train_c.items(), key=operator.itemgetter(0)))]
+    vlabels, vvalues = [list(m) for m in zip(*sorted(valid_c.items(), key=operator.itemgetter(0)))]
+    width = 0.5; indices = np.arange(len(tlabels))
+    plt.bar(indices, tvalues, width=width, 
+            color='b', label='Training')
+    plt.bar([i+0.25*width for i in indices], vvalues, 
+            width=0.5*width, color='r', alpha=0.5, label='Validation')
+    plt.xticks(indices+width/2., 
+               ['{}'.format(i) for i in range(len(tvalues))] )
+    plt.xticks(rotation=0 if nc<11 else 90)
+    plt.legend()
+    plt.show()
